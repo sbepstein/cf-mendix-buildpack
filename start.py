398cf8ee27e58377844422a8000e5caf2b378d18
Xiwen Cheng <xiwen.cheng@mendix.com>
2015-11-29 08:26:26 +0100

    WIP

--- start.py
+++ start.py
@@ -502,5 +502,6 @@ if __name__ == '__main__':
     #configure_debugger(m2ee)
     start_fastpush_controller()
     start_nginx()
+    print os.listdir("./")
     loop_forever()
     #loop_until_process_dies(m2ee)
