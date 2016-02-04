a8004d0f8ced0944927b70712d33ed3c5962dde3
Jouke Waleson <jouke.waleson@mendix.com>
2016-02-04 12:23:03 +0100

    debug

--- start.py
+++ start.py
@@ -386,7 +386,7 @@ def set_up_m2ee_client(vcap_data):
     set_application_name(m2ee, vcap_data['application_name'])
     felix = os.path.join(os.getcwd(), 'runtimes/6.2.0/runtime/felixconfig.properties.template')
     subprocess.call(['sed', '-i', "/sun.nio.cs/d", felix])
-    subprocess.call("echo '\tsun.nio.cs,\' >> " + felix, shell=True)
+    subprocess.call("echo '\tsun.nio.cs,\\' >> " + felix, shell=True)
     subprocess.call("echo '\tcom.singularity' >> " + felix, shell=True)
     subprocess.call(["cat", felix])
 
