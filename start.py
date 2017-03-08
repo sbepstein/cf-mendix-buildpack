3764664ab2e705e9ba91b9f768031d41b5015b94
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2017-03-08 10:57:30 +0100

    Hello, world

--- start.py
+++ start.py
@@ -20,7 +20,7 @@ from buildpackutil import i_am_primary_instance
 
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
 
-logger.info('Started Mendix Cloud Foundry Buildpack')
+logger.info('Started Mendix Cloud Foundry Buildpack PBAR-amplified')
 
 logging.getLogger('m2ee').propagate = False
 
